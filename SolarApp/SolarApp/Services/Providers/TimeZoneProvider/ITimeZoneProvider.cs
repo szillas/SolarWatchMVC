35248d61efb82fa346a3fe654a6361312ec3cbc3
Laszlo Szijarto <szillas92@gmail.com>
2024-06-05 15:14:11 +0200

    Created ITimeZoneProvider interface and its implementation, TimeApiProvider class. This class has a GetTimeZone public method, that calls the TimeApi with coordinates. Inside the response, the local timezone is available.

---
+++ SolarApp/SolarApp/Services/Providers/TimeZoneProvider/ITimeZoneProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace SolarApp.Services.Providers.TimeZoneProvider;
+
+public interface ITimeZoneProvider
+{
+    Task<string> GetTimeZone(double latitude, double longitude);
+}
---
+++ SolarApp/SolarApp/Services/Providers/TimeZoneProvider/TimeApiProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace SolarApp.Services.Providers.TimeZoneProvider;
+
+public class TimeApiProvider : ITimeZoneProvider
+{
+    private readonly ILogger<TimeApiProvider> _logger;
+
+    public TimeApiProvider(ILogger<TimeApiProvider> logger)
+    {
+        _logger = logger;
+    }
+    
+    public async Task<string> GetTimeZone(double latitude, double longitude)
+    {
+        var url = $"https://timeapi.io/api/TimeZone/coordinate?latitude={latitude}&longitude={longitude}";
+
+        using var client = new HttpClient();
+
+        _logger.LogInformation("Calling TimeApi API for TimeZone with url: {url}", url);
+
+        var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
+    }
+}
