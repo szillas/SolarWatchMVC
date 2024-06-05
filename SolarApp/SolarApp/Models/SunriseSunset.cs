0cd035f8a7a1ee91b9468d76b86f264df790b573
Laszlo Szijarto <szillas92@gmail.com>
2024-06-05 10:01:01 +0200

    Created SunriseSunset Model.

---
+++ SolarApp/SolarApp/Models/SunriseSunset.cs
@@ -0,0 +1,11 @@
+﻿namespace SolarApp.Models;
+
+public class SunriseSunset
+{
+    public int Id { get; init; }
+    public DateTime Date { get; set; }
+    public City City { get; set; }
+    public string Sunrise { get; set; }
+    public string Sunset { get; set; }
+    public string? TimeZone { get; set; }
+}
