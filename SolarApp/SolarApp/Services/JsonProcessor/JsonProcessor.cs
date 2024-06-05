f230069ade177f4d8b6b5deba79b013a079372b8
Laszlo Szijarto <szillas92@gmail.com>
2024-06-05 16:19:37 +0200

    Started refactoring the jsonprocessor. Moved the WeatherApiProcessor method to a new interface and implementation.

--- SolarApp/SolarApp/Services/JsonProcessor/JsonProcessor.cs
+++ SolarApp/SolarApp/Services/JsonProcessor/JsonProcessor.cs
@@ -83,22 +83,6 @@ public class JsonProcessor : IJsonProcessor
 
         return date.ToString("HH:mm:ss");
     }
-
-    private DateTime DateParser(string? date)
-    {
-        if (string.IsNullOrEmpty(date))
-        {
-            return DateTime.Today;
-        }
-        else
-        {
-            if(!DateTime.TryParse(date, out var dateTime))
-            {
-                throw new FormatException("Date format is not correct.");
-            }
-            return dateTime;
-        }
-    }
     
     private string GetStringProperty(JsonElement element, string propertyName)
     {
---
+++ SolarApp/SolarApp/Services/JsonProcessor/WeatherApi/IWeatherApiProcessor.cs
@@ -0,0 +1,8 @@
+﻿using SolarApp.Models;
+
+namespace SolarApp.Services.JsonProcessor.WeatherApi;
+
+public interface IWeatherApiProcessor
+{
+    Task<City> ProcessWeatherApiCityStringToCity(string data);
+}
