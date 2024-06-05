dae35bbbda21bb8c2684c7af260646739252cd88
Laszlo Szijarto <szillas92@gmail.com>
2024-06-05 09:59:56 +0200

    Created city Model.

---
+++ SolarApp/SolarApp/Models/City.cs
@@ -0,0 +1,11 @@
+﻿namespace SolarApp.Models;
+
+public class City
+{
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string? State { get; set; }
+    public string Country { get; set; }
+}
