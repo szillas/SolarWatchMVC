ee79e1a84ee32148de57b979a5086ef3cf43dcce
Laszlo Szijarto <szillas92@gmail.com>
2024-06-05 10:23:22 +0200

    Added ICoordinateProvider interface and OpenWeatherCoordinateProvider class to handle external api call to openweatermap.

---
+++ SolarApp/SolarApp/Services/Providers/CoordinateProvider/ICoordinateProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace SolarApp.Services.Providers.CoordinateProvider;
+
+public interface ICoordinateProvider
+{
+    Task<string> GetCityFromOpenWeatherMap(string city);
+    Task<string> GetCityFromOpenWeatherMap(string city, string country);
+    Task<string> GetCityFromOpenWeatherMap(string city, string country, string state);
+}
---
+++ SolarApp/SolarApp/Services/Providers/CoordinateProvider/OpenWeatherCoordinateProvider.cs
@@ -0,0 +1,50 @@
+﻿namespace SolarApp.Services.Providers.CoordinateProvider;
+
+public class OpenWeatherCoordinateProvider : ICoordinateProvider
+{
+    private readonly ILogger<OpenWeatherCoordinateProvider> _logger;
+    private readonly string _openWeatherApiKey;
+    
+    public OpenWeatherCoordinateProvider(ILogger<OpenWeatherCoordinateProvider> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _openWeatherApiKey = configuration["SolarWatch:OpenWeatherMapKey"] ?? throw new InvalidOperationException();
+    }
+
+    public async Task<string> GetCityFromOpenWeatherMap(string city)
+    {
+        var url = $"https://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={_openWeatherApiKey}";
+
+        using var client = new HttpClient();
+
+        _logger.LogInformation("Calling OpenWeather API for Coordinates with url: {url}", url);
+
+        var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
+    }
+    
+    public async Task<string> GetCityFromOpenWeatherMap(string city, string country)
+    {
+        var url = $"https://api.openweathermap.org/geo/1.0/direct?q={city},{country}&limit=1&appid={_openWeatherApiKey}";
+
+        using var client = new HttpClient();
+
+        _logger.LogInformation("Calling OpenWeather API for Coordinates with url: {url}", url);
+
+        var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
+    }
+    
+    public async Task<string> GetCityFromOpenWeatherMap(string city, string country, string state)
+    {
+        var url = 
+            $"https://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=1&appid={_openWeatherApiKey}";
+
+        using var client = new HttpClient();
+
+        _logger.LogInformation("Calling OpenWeather API for Coordinates with url: {url}", url);
+
+        var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
+    }
+}
